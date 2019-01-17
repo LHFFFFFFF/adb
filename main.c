f93acc57e900b1bec33e33290e587030de4537b1
Your Name <you@example.com>
2019-01-17 15:33:58 +0800

    第4功能

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<math.h>
 
 struct Point
 {
@@ -77,6 +78,32 @@ int main()
         }
         if(code == 4)
         {
+            printf("qingshurudangqianweizhizuobiao: \n");
+            int x;
+            int y;
+            scanf("%d",&x);
+            scanf("%d",&y);
+
+            double min = 100000;
+            int carID = 0;
+            for(int i=0;i<index;i++){
+                int dx = x - arr[i].x;
+                int dy = y - arr[i].y;
+                double l = sqrt(dx*dx+dy*dy);
+
+                if(min > l){
+                    min = l;
+                    carID = i;
+                }
+            }
+
+            printf("yonghudangqianweizhiyudi%dgechedejuliwei%1f,shizuixiaojuli\n",carID+1,min);
+
+
+            printf("\ndianjihuichejixu\n");
+            char xx;
+            scanf("%c",&xx);
+            scanf("%c",&xx);            
         }
         if(code == 5)
         {
