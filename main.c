0175d0981d5458e7907d6cc8c982c6e173a03b18
Your Name <you@example.com>
2019-01-17 14:53:36 +0800

    位置添加功能

--- main.c
+++ main.c
@@ -1,8 +1,62 @@
 #include <stdio.h>
 
-int main(){
+struct Point
+{
+    int x;
+    int y;
+};
 
-    printf("hello world!");
+int main()
+{
+    int index = 0;
+    struct Point arr[100];
+    printf("huanyingjinruzhuanchangweizhiguanlixitong!\n");
+
+    while (1)
+    {
+        printf("1---tianjiacheliangweizhi\n");
+        printf("2---shanchucheliangweizhi\n");
+        printf("3---xianshiquanbucheliangweizhi\n");
+        printf("4---fanhuiliwodeweizhizuijindecheliang\n");
+        printf("5---tuichu\n");
+
+        printf("qingxuanzexiangyinggongneng\n");
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1)
+        {
+            printf("qingshurucheliangweizhidexzuobiaoheyzuobiao:\n");
+            int x;
+            int y;
+            scanf("%d",&x);
+            scanf("%d",&x);
+
+            arr[index].x = x;
+            arr[index].y = y;
+
+            index++;
+
+            printf("tianjiawancheng,dianjihuichejixu\n");
+            char xx;
+            scanf("%c",&xx);
+            scanf("%c",&xx);
+        }
+        if(code == 2)
+        {
+        }
+        if(code == 3)
+        {
+        }
+        if(code == 4)
+        {
+        }
+        if(code == 5)
+        {
+            printf("yingyongyituichu\n");
+            break;
+        }
+    }
 
     return 0;
 
